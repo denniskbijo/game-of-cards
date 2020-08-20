780196f75dbf7f922dfe0f1a1bc43f7dbf521649
dennisctb <dennis@catchthatbus.com>
2020-08-20 14:59:20 +0530

    Updated Main Game rules

--- src/main/java/com/goc/MainGame.java
+++ src/main/java/com/goc/MainGame.java
@@ -5,7 +5,26 @@ import java.util.List;
 import java.util.Scanner;
 
 import com.goc.beans.CardDeck;
+import com.goc.beans.Player;
 
+/**
+ * Basic Rules: - Use a standard deck of cards (no Joker). - Each player is
+ * dealt only three cards. - 'A' is considered to have a number value of 1. -
+ * 'A' is considered the top card in a face-off. So the order is A > K > Q > J >
+ * 10...2
+ * 
+ * Victory: - A trail (three cards of the same number) is the highest possible
+ * combination. - The next highest is a sequence (numbers in order, e.g., 4,5,6.
+ * A is considered to have a value of 1). - The next highest is a pair of cards
+ * (e.g.: two Kings or two 10s). - If all else fails, the top card (by number
+ * value wins). - If the top card has the same value, each of the tied players
+ * draws a single card from the deck until a winner is found. - Only the newly
+ * drawn cards are compared to decide a tie. The top card wins a tie. - For now
+ * the suit (spades/hearts etc...), does not matter.
+ * 
+ * @author denniskbijo
+ *
+ */
 public class MainGame {
 
 	public static void main(String[] args) {
@@ -21,7 +40,12 @@ public class MainGame {
 			players.add(new Player(scan.next()));
 		}
 
-		System.out.println();
+		System.out.println("The Players are ready!");
+		for (Player player : players) {
+			System.out.println(player);
+		}
+		// Deal the cards
+
 	}
 
 }
