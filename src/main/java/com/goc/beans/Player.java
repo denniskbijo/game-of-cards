d40d5ee94fc61c323a24c5cf4031805b3a0ee481
dennisctb <dennis@catchthatbus.com>
2020-08-20 22:26:06 +0530

    Check if all the cards are in sequence

--- src/main/java/com/goc/beans/Player.java
+++ src/main/java/com/goc/beans/Player.java
@@ -79,12 +79,48 @@ public class Player {
 		return handRank;
 	}
 
+	/**
+	 * Check if all the cards are in sequence.
+	 * 
+	 * @param hand
+	 * @return
+	 */
+	private boolean isSequence(List<Card> hand) {
+		boolean isSequence = true;
+		// Check if its a Sequence (Consecutive ranks)
+
+		// Set the prevRank to a negative rank
+		int prevRank = -1;
+
+		// Iterate through the hand
+		for (int i = 0; i < hand.size(); i++) {
+			// Get rank of card
+			int currentRank = hand.get(i).getRank().getValue();
+			// Check if the rank of card is exactly one more than the previous one.
+
+			if (prevRank < 0 || currentRank == (prevRank + 1)) {
+				// If prevRank is negative , then this is the first card and if the rank of card
+				// is just before the rank of the previous card, a sequence is possible.
+				prevRank = currentRank;
+			} else {
+				return false;
+			}
+		}
+		return isSequence;
+	}
+
+	/**
+	 * Check if all the cards are of same rank i.e A Trail.
+	 * 
+	 * @param cards
+	 * @return
+	 */
 	private boolean isTrail(List<Card> cards) {
 		boolean isTrail = true;
-		//Check if its a Trail (same rank) 
+		// Check if its a Trail (same rank)
 		Rank rank = cards.get(0).getRank();
-		for(Card card: cards) {
-			if(!rank.equals(card.getRank())) {
+		for (Card card : cards) {
+			if (!rank.equals(card.getRank())) {
 				isTrail = false;
 				break;
 			}
