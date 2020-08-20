6e7796f858c0aed5aa6bcaf6320b451f9880572f
dennisctb <dennis@catchthatbus.com>
2020-08-20 13:31:12 +0530

    Added Player class

---
+++ src/main/java/com/goc/beans/Player.java
@@ -0,0 +1,42 @@
+package com.goc.beans;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents a player in the Game of Cards
+ * 
+ * @author denniskbijo
+ *
+ */
+class Player {
+
+	private String name;
+
+	private List<Card> cards = new ArrayList<>();
+
+	Player(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	List<Card> getCards() {
+		return cards;
+	}
+
+	public void setCards(List<Card> cards) {
+		this.cards = cards;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+}
