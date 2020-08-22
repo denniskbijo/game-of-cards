20f2ca0b1b99e8a6f0f065bc1e302172a80e8186
dennisctb <dennis@catchthatbus.com>
2020-08-22 11:15:12 +0530

    Added MainGameTest

---
+++ src/test/java/com/goc/test/MainGameTest.java
@@ -0,0 +1,57 @@
+package com.goc.test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import com.goc.beans.CardDeck;
+import com.goc.beans.Player;
+
+class MainGameTest {
+
+	private static List<Player> testPlayers = null;
+
+	private static CardDeck cardDeck = null;
+
+	@BeforeAll
+	static void initAll() {
+		//Initialise test players
+		testPlayers = new ArrayList<>();
+		Player ronaldo = new Player("C Ronaldo");
+		Player messi = new Player("L Messi");
+		Player henry = new Player("T O'Henry");
+		Player zidane = new Player("Z Zidane");
+		testPlayers.add(ronaldo);
+		testPlayers.add(messi);
+		testPlayers.add(henry);
+		testPlayers.add(zidane);
+		// Initialise Card Deck
+		cardDeck = new CardDeck();
+	}
+
+
+	@Test
+	void testIdentifyWinner() {
+	}
+
+	@Test
+	void testCannotIdentifyWinner() {
+		fail("a failing test");
+	}
+
+	@Test
+	@Disabled("Only for Tiebreaker case")
+	void testTieBreaker() {
+
+	}
+
+	@AfterAll
+	static void tearDownAll() {
+	}
+}
